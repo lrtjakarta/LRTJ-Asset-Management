0299709b1773e08939e673a8c10d13da0580b876
Parzip4l <sobirinjamo33@gmail.com>
2026-03-09 11:03:55 +0700

    cek

--- resources/views/assets/assets.blade.php
+++ resources/views/assets/assets.blade.php
@@ -438,8 +438,8 @@
                 <ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-7 my-0 pt-1">
                     <!--begin::Item-->
                     <li class="breadcrumb-item text-muted">
-                        Assets
-                    </li>
+                        Assets Data
+                    </li> 
                     <!--end::Item-->
                 </ul>
                 <!--end::Breadcrumb-->
